b988f3f25fe36ac8b5a374dcade683c48a96e653
lucastack <lucas.suarez@gmail.com>
2024-12-15 17:50:16 -0300

    fix typo in os.listdir

--- src/serve/main.py
+++ src/serve/main.py
@@ -24,7 +24,7 @@ def load_model():
     global model
     model_path = os.environ.get("AIP_STORAGE_URI", "/model")
     logging.info("Loading model artifacts from:", model_path)
-    logging.info(f"Artifacts: {os.listidr(model_path)}")
+    logging.info(f"Artifacts: {os.listdr(model_path)}")
     encoder_path = os.path.join(model_path, "brands_encoder.pkl")
     brands_encoder = joblib.load(encoder_path)
 
